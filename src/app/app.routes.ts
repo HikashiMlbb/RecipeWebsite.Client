2c18f3ed0b28b4f02eee90ee705db1b1b8344106
hikashi <hikashimlbb@gmail.com>
2025-02-17 15:47:08 +0700

    Created Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { homeRoutes as HomeRoutes } from './home/home.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", 
+        children: HomeRoutes
+    }
+];
