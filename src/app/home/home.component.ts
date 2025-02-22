38f7270142827fc19cd83ff2fe7da725a1f73930
hikashi <hikashimlbb@gmail.com>
2025-02-22 13:17:25 +0700

    Moved RecipesLayout to the other component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RecipesLayoutComponent } from "../recipes-layout/recipes-layout.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RecipesLayoutComponent],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
---
+++ src/app/recipes-layout/recipes-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-layout',
+  imports: [],
+  templateUrl: './recipes-layout.component.html',
+  styleUrl: './recipes-layout.component.css'
+})
+export class RecipesLayoutComponent {
+
+}
