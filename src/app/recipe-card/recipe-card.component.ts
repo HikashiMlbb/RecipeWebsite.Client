7fa43af430b15e5a366a3b37b60da477675b6c12
hikashi <hikashimlbb@gmail.com>
2025-02-22 14:23:35 +0700

    Added RecipePreview component

---
+++ src/app/recipe-card/recipe-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card',
+  imports: [],
+  template: `
+    <p>
+      recipe card works!
+    </p>
+  `,
+  styles: ``
+})
+export class RecipeCardComponent {
+
+}
