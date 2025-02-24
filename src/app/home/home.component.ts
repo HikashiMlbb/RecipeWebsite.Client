aa6d2d9fe7d700eca46694efbc90e7c97e6e1d88
hikashi <hikashimlbb@gmail.com>
2025-02-24 11:55:42 +0700

    Added smooth scroll up on click to home link

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterEvent, RouterLink, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { RecipesLayoutComponent } from "../recipes-layout/recipes-layout.component";
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +9,19 @@ import { RecipesLayoutComponent } from "../recipes-layout/recipes-layout.compone
   imports: [RouterLink, RecipesLayoutComponent],
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private currentPath!: String
+  private routerEventSubscription!: Subscription;
+  
+  constructor (private router: Router, private location: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.routerEventSubscription = this.router.events
+      .pipe(filter(x => x instanceof NavigationEnd))
+      .subscribe(() => window.scrollTo({ top: 0, behavior: 'smooth'}));
+  }
+
+  ngOnDestroy(): void {
+    this.routerEventSubscription.unsubscribe();
+  }
 }
--- src/app/recipes-layout/recipes-layout.component.ts
+++ src/app/recipes-layout/recipes-layout.component.ts
@@ -41,6 +41,78 @@ export class RecipesLayoutComponent {
       difficulty: 1,
       cookingTime: 155,
       rating: 4.3
+    },
+    {
+      id: 1,
+      title: "Паста Болоньезе",
+      image: "https://cdn1.ozonusercontent.com/s3/club-storage/images/article_image_1632x1000/685/f6d7c2eb-4616-4d99-bdf0-0d28b62f2dce.jpeg",
+      difficulty: 2,
+      cookingTime: 155,
+      rating: 4.7
+    },
+    {
+      id: 2,
+      title: "Томатный Суп",
+      image: "https://lifehacker.ru/wp-content/uploads/2018/05/tomatnyj-sup_1525442518.jpg",
+      difficulty: 3,
+      cookingTime: 155,
+      rating: 4.4
+    },
+    {
+      id: 4,
+      title: "Классическая яичница-глазунья",
+      image: "https://s1.eda.ru/StaticContent/Photos/2/ea/2ea2a5739495455ebe732e8c42cbe453.jpg",
+      difficulty: 1,
+      cookingTime: 155,
+      rating: 4.3
+    },
+    {
+      id: 1,
+      title: "Паста Болоньезе",
+      image: "https://cdn1.ozonusercontent.com/s3/club-storage/images/article_image_1632x1000/685/f6d7c2eb-4616-4d99-bdf0-0d28b62f2dce.jpeg",
+      difficulty: 2,
+      cookingTime: 155,
+      rating: 4.7
+    },
+    {
+      id: 2,
+      title: "Томатный Суп",
+      image: "https://lifehacker.ru/wp-content/uploads/2018/05/tomatnyj-sup_1525442518.jpg",
+      difficulty: 3,
+      cookingTime: 155,
+      rating: 4.4
+    },
+    {
+      id: 4,
+      title: "Классическая яичница-глазунья",
+      image: "https://s1.eda.ru/StaticContent/Photos/2/ea/2ea2a5739495455ebe732e8c42cbe453.jpg",
+      difficulty: 1,
+      cookingTime: 155,
+      rating: 4.3
+    },
+    {
+      id: 1,
+      title: "Паста Болоньезе",
+      image: "https://cdn1.ozonusercontent.com/s3/club-storage/images/article_image_1632x1000/685/f6d7c2eb-4616-4d99-bdf0-0d28b62f2dce.jpeg",
+      difficulty: 2,
+      cookingTime: 155,
+      rating: 4.7
+    },
+    {
+      id: 2,
+      title: "Томатный Суп",
+      image: "https://lifehacker.ru/wp-content/uploads/2018/05/tomatnyj-sup_1525442518.jpg",
+      difficulty: 3,
+      cookingTime: 155,
+      rating: 4.4
+    },
+    {
+      id: 4,
+      title: "Классическая яичница-глазунья",
+      image: "https://s1.eda.ru/StaticContent/Photos/2/ea/2ea2a5739495455ebe732e8c42cbe453.jpg",
+      difficulty: 1,
+      cookingTime: 155,
+      rating: 4.3
     }
   ];
 }
