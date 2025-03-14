b672917d74e3f69a892b7c77eaa6e51178a5c75e
hikashi <hikashimlbb@gmail.com>
2025-03-14 09:41:47 +0700

    Added recipe form component

---
+++ src/app/recipe-form/recipe-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-form',
+  imports: [],
+  template: `
+    <p>
+      recipe-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class RecipeFormComponent {
+
+}
